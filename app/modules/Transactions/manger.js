a7cf88fe6c1e3dbb818989b72f303ac636ca19ed
kajal_KajalBobal <kajal@leewayhertz.com>
2022-01-05 16:47:37 +0530

    sorting in transaction for adress api

--- app/modules/Transactions/manger.js
+++ app/modules/Transactions/manger.js
@@ -1,21 +1,30 @@
 import Utils from '../../utils'
 import TransactionModel from "../../models/transaction";
 import TransactionHistoryModel from "../../models/historical";
-import {apiFailureMessage} from '../../common/constants';
+import { apiFailureMessage } from '../../common/constants';
 
 export default class Manger {
     getTransactionsForAddress = async (pathParameter, queryStringParameter) => {
 
         Utils.lhtLog("BLManager:getAccountDetailsUsingAddress", "getAccountDetailsUsingAddress started", "", "");
 
-        let skip = parseInt(0)
-        let limit = parseInt(1)
+        let skip = parseInt(0),
+            limit = parseInt(1),
+            sortKey = "blockNumber",
+            sortType = -1;
         let address = pathParameter && pathParameter.address.toLowerCase();
         if (queryStringParameter && queryStringParameter.skip)
             skip = parseInt(queryStringParameter.skip)
 
         if (queryStringParameter && queryStringParameter.limit)
             limit = parseInt(queryStringParameter.limit)
+
+        if (queryStringParameter && queryStringParameter.sortKey)
+            sortKey = queryStringParameter.sortKey
+
+        if (queryStringParameter && queryStringParameter.sortType)
+            sortType = parseInt(queryStringParameter.sortType)
+
         let responseTransaction = []
 
         Utils.lhtLog("BLManager:getAccountDetailsUsingAddress", "get total transaction count", "", "");
@@ -25,30 +34,35 @@ export default class Manger {
                 {
                     $and: [{
                         to: address
-                    }, {hash: {$regex: ".*" + keyword + ".*", $options: 'i'}}]
+                    }, { hash: { $regex: ".*" + keyword + ".*", $options: 'i' } }]
                 }, "", skip, limit, {});
             let fromTransaction = await TransactionModel.getTransactionList(
                 {
                     $and: [{
                         from: address
-                    }, {hash: {$regex: ".*" + keyword + ".*", $options: 'i'}}]
+                    }, { hash: { $regex: ".*" + keyword + ".*", $options: 'i' } }]
                 }, "", skip, limit, {});
             responseTransaction = [...fromTransaction, ...toTransaction]
         } else {
             Utils.lhtLog("BLManager:getAccountDetailsUsingAddress", "get total transaction without keyword", "", "");
 
             let fromTransaction = await TransactionModel.getTransactionList(
-                {from: address}, "", skip, limit, {blockNumber:-1}
+                { from: address }, "", skip, limit, { [sortKey]: sortType }
             )
             let toTransaction = await TransactionModel.getTransactionList(
-                {to: address}, "", skip, limit, {blockNumber:-1}
+                { to: address }, "", skip, limit, { [sortKey]: sortType }
             )
             // return toTransaction;
             responseTransaction = [...fromTransaction, ...toTransaction]
 
+            responseTransaction = responseTransaction.sort((transaction1, transaction2) => {
+                if (sortType === 1)
+                    return (transaction1[sortKey] - transaction2[sortKey])
+                else
+                    return (transaction2[sortKey] - transaction1[sortKey])
+            })
         }
-        return responseTransaction;
-
+        return responseTransaction.slice(limit);
     }
 
     getLatestTransactions = async (req) => {
@@ -75,7 +89,7 @@ export default class Manger {
                     to: address
                     // $or: [{ to: address },
                     // { from: address }]
-                }, {hash: {$regex: ".*" + keyword + ".*", $options: 'i'}}
+                }, { hash: { $regex: ".*" + keyword + ".*", $options: 'i' } }
                 ]
             }))
             fromCount = Promise.resolve(TransactionModel.countData({
@@ -83,15 +97,15 @@ export default class Manger {
                     from: address
                     // $or: [{ to: address },
                     // { from: address }]
-                }, {hash: {$regex: ".*" + keyword + ".*", $options: 'i'}}
+                }, { hash: { $regex: ".*" + keyword + ".*", $options: 'i' } }
                 ]
             }))
         } else {
             Utils.lhtLog("BLManager:getTransactionsCountForAddress", "get total transaction count for address without keyword", "", "");
 
 
-            fromCount = Promise.resolve(TransactionModel.countData({from: address}))
-            toCount = Promise.resolve(TransactionModel.countData({to: address}))
+            fromCount = Promise.resolve(TransactionModel.countData({ from: address }))
+            toCount = Promise.resolve(TransactionModel.countData({ to: address }))
         }
         await fromCount.then((data) => {
             totalCount += data
@@ -104,16 +118,16 @@ export default class Manger {
     }
 
     getSomeDaysTransactions = async (params) => {
-        let selectionKey = {day: 1, transactionCount: 1, avgGasPrice: 1, timestamp:1}
+        let selectionKey = { day: 1, transactionCount: 1, avgGasPrice: 1, timestamp: 1 }
         Utils.lhtLog("BLManager:getTotalTransactions", "get getSomeDaysTransactions  count", "", "");
-        return await TransactionHistoryModel.getHistoricalDataList({}, selectionKey, 0, parseInt(params.days), {timestamp: -1});
+        return await TransactionHistoryModel.getHistoricalDataList({}, selectionKey, 0, parseInt(params.days), { timestamp: -1 });
     }
 
     getTransactionDetailsUsingHash = async (params) => {
-        let hash=params.hash.toLowerCase();
-        const transaction = await TransactionModel.getTransaction({hash: hash});
+        let hash = params.hash.toLowerCase();
+        const transaction = await TransactionModel.getTransaction({ hash: hash });
         if (!transaction)
-            throw {message: apiFailureMessage.NO_TRANSACTION}
+            throw { message: apiFailureMessage.NO_TRANSACTION }
         return transaction;
 
     }
