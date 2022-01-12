2ba6eddae2fe595873ad3ea2a073bedb6f2052ee
ayushkul <akcool.ayush.svm@gmail.com>
2022-01-12 18:28:12 +0530

    getListOfTransferTransactionsForToken updated for search

--- app/modules/TransferTransactions/index.js
+++ app/modules/TransferTransactions/index.js
@@ -3,14 +3,14 @@ import { apiSuccessMessage, httpConstants } from '../../common/constants'
 import BLManager from './manger'
 
 export default class Index {
- 
+
   async getListOfTransferTransactionsForToken (request , response) {
     Utils.lhtLog('Inside getListOfTransferTransactionsForToken', request.params, 'getListOfTransferTransactionsForToken', 0, '')
-    const [error, getMetersRes] = await Utils.parseResponse(new BLManager().getListOfTransferTransactionsForToken(request.params, request.query))
+    const [error, getMetersRes] = await Utils.parseResponse(new BLManager().getListOfTransferTransactionsForToken({...request.params, ...request.query}))
     if (!getMetersRes) { return Utils.handleError(error, request, response) }
     return Utils.response(response, getMetersRes, apiSuccessMessage.FETCH_SUCCESS, httpConstants.RESPONSE_STATUS.SUCCESS, httpConstants.RESPONSE_CODES.OK)
   }
-  
+
   async getTotalTransferTransactionForToken (request , response) {
     Utils.lhtLog('Inside getListOfTransferTransactionsForToken', request.params, 'getListOfTransferTransactionsForToken', 0, '')
     const [error, getMetersRes] = await Utils.parseResponse(new BLManager().getTotalTransferTransactionForToken(request.params))
