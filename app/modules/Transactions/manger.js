c5654efdd74b4e5c5233493f31c788d132df5bc8
Deepak Tomar <deepakt@leewayhertz.com>
2022-01-18 16:29:22 +0530

    modified address-stats API

--- app/modules/Transactions/manger.js
+++ app/modules/Transactions/manger.js
@@ -195,7 +195,7 @@ export default class Manger {
         Utils.lhtLog("BLManager:getAddressStats", "getAddressFromStats ", fromStats && fromStats.length > 0 && fromStats[0].avgTransactions.length, "");
 
         let totalAverage = [...(toStats && toStats.length > 0 &&toStats[0].avgTransactions ? toStats[0].avgTransactions:[]), ...(fromStats && fromStats.length > 0 && fromStats[0].avgTransactions? fromStats[0].avgTransactions:[])]
-        totalAverage.sort((a, b) => a.timestamp > b.timestamp ? 1 : -1);
+        totalAverage.sort((a, b) => a.timestamp > b.timestamp ? -1 : 1);
         let highestAndAvgBalance = this.calculateBalance(totalAverage, addressDetails.balance)
         let gasFee =( toStats && toStats.length > 0 &&toStats[0].gasFee && toStats[0].gasFee) + (fromStats && fromStats.length >0 && fromStats[0].gasFee&& fromStats[0].gasFee);
 
